95b9e5d3a8c3546713713316de25a5928f1f9461
hewenjian <591981210@qq.com>
2018-11-26 23:45:40 +0800

    可准确打印报错位置

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+var ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
     entry: {
         app: './src/index.js',
         print: './src/print.js'
     },
+    devtool: 'inline-source-map',
     plugins: [
+        new ManifestPlugin(),
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Output Management'
