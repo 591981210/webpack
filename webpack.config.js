6b6bf9ea68fefda07f76b5db40fb437f72704a06
hewenjian <591981210@qq.com>
2018-11-27 12:02:07 +0800

    1,生产模式压缩代码.2.自动去掉未使用的代码,测试未成功

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-var ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
-    entry: {
-        app: './src/index.js',
-        print: './src/print.js'
-    },
+    entry: './src/index.js',
     devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist'
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     },
     plugins: [
-        new ManifestPlugin(),
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Output Management'
         })
     ],
     output: {
-        filename: '[name].bundle.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
     },
+    mode: "development"//production,development
 };
