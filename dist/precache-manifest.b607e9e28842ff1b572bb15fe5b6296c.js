4056cf80fbf1a5ceea6a9889f985a5308a5e0916
hewenjian <591981210@qq.com>
2018-11-27 19:46:26 +0800

    渐进式网络应用程序---占未测试成功

---
+++ dist/precache-manifest.b607e9e28842ff1b572bb15fe5b6296c.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "d610b970428079d81eb2",
+    "url": "print.bundle.js"
+  },
+  {
+    "revision": "4e051cefff5301973505fa2026506a82",
+    "url": "index.html"
+  },
+  {
+    "revision": "df19bca602afa551708a",
+    "url": "app.bundle.js"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import _ from 'lodash';
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(registration => {
+            console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
+
 function component() {
     var element = document.createElement('div');
     var button = document.createElement('button');
@@ -10,13 +20,6 @@ function component() {
     element.appendChild(br);
     element.appendChild(button);
 
-    // Note that because a network request is involved, some indication
-    // of loading would need to be shown in a production-level site/app.
-    button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
-        var print = module.default;
-
-        print();
-    });
 
     return element;
 }
